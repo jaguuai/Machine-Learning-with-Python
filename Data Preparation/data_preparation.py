c30bdb7eab932bc9e203372adc4489c37cb79656
Ayşe Yılmaz <ayseeyyilmaz96@gmail.com>
2024-04-22 12:05:27 +0300

    up

---
+++ Data Preparation/data_preparation.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 22 11:53:30 2024
+
+@author: ADMIN
+"""
+#libs
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+#codes
+#data upload
+data=pd.read_csv("data.csv")
+print(data)
+
