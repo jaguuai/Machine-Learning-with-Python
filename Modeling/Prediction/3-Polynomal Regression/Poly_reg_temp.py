9e705244defdfd166081fd13e50ac1cee9b72461
Ayşe Yılmaz <ayseeyyilmaz96@gmail.com>
2024-04-30 10:30:49 +0300

    poly_reg

--- Modeling/Prediction/3-Polynomal Regression/Poly_reg_temp.py
+++ Modeling/Prediction/3-Polynomal Regression/Poly_reg_temp.py
@@ -1,28 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 30 10:20:19 2024
+Polynomal regression templete
+@author: ADMIN
+"""
 # 1-Libs
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 # 2-Data preprocessing
 # 2.1-Data Upload
+# 2.2-Data Frame Slice (Dilimleme)
 data=pd.read_csv("salaries.csv")
 x=data.iloc[:,1:2]
-X=x.values
-print(x)
 y=data.iloc[:,2:]
+# 2.3-Numpy array conversion
+X=x.values
 Y=y.values
+
 # linear regression
+# linear regresyon 1. derecelidir
 from sklearn.linear_model import LinearRegression
 lin_reg=LinearRegression()
 lin_reg.fit(X,Y)
 
-plt.scatter(X,Y)
-plt.plot(x,lin_reg.predict(X),color="red")
-plt.show()
-# linear regresyon 1. derecelidir
-# *****************************************
-# polynomal regression
+# polynomal regression(nonlinear model)
 # Aslında çok değişkenli çok dereceli linear regresyon gibi düşünülebilir
 # denkleminde gördüğümüz üzere
+
 from sklearn.preprocessing import PolynomialFeatures
 poly_reg=PolynomialFeatures(degree=2)
 #polinom objesi oluşturur
@@ -34,31 +39,32 @@ from sklearn.linear_model import LinearRegression
 lin_reg2=LinearRegression()
 lin_reg2.fit(x_poly,y)
 # x x^2 x^3 al beta kat sayılarını bul demektir
+
+# *************************
+# polynomial equation with more degrees
+poly_reg3=PolynomialFeatures(degree=4)
+x_poly3=poly_reg3.fit_transform(X)
+
+
+lin_reg3=LinearRegression()
+lin_reg3.fit(x_poly3,y)
+
+# data visualition
+plt.scatter(X,Y)
+plt.plot(x,lin_reg.predict(X),color="red")
+plt.show()
+
+
 plt.scatter(X,Y,color="red")
 # x,y data point
 plt.plot(x,lin_reg2.predict(poly_reg.fit_transform(X)),color="blue")
 # lin_reg i predict etmek ve gelen değeri polinomal domain e cevirmek
 plt.show()
 
-# *************************
 
-poly_reg=PolynomialFeatures(degree=4)
-#polinom objesi oluşturur
-x_poly=poly_reg.fit_transform(X)
-# linear dünyadaki X i polinomal dünyayaa çevirme
-print(x_poly)
-
-
-lin_reg2=LinearRegression()
-lin_reg2.fit(x_poly,y)
-# x x^2 x^3 al beta kat sayılarını bul demektir
 plt.scatter(X,Y,color="green")
-# x,y data point
-plt.plot(x,lin_reg2.predict(poly_reg.fit_transform(X)),color="black")
-# lin_reg i predict etmek ve gelen değeri polinomal domain e cevirmek
+plt.plot(x,lin_reg3.predict(poly_reg3.fit_transform(X)),color="black")
 plt.show()
-# Eğer dereceyi arttırırsak  tahmin daha da iyileşiyor
+
 # predictions
 print(lin_reg2.predict(poly_reg.fit_transform([[6.6]])))
-
-
